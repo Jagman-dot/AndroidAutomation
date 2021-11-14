07a268b296993fabf8a6dbcbad438918436436af
Jagmandeep Dhaliwal <jagmandeepdhaliwal@Jagmandeeps-MacBook-Pro.local>
2021-11-14 15:52:04 -0500

    able to send apk file to the eumlator

--- src/test/java/Base.java
+++ src/test/java/Base.java
@@ -1,11 +1,17 @@
+import io.appium.java_client.android.Activity;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import sun.security.krb5.internal.crypto.Des;
 
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Base {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException {
 
 
     // app that we want to automate
@@ -13,15 +19,29 @@ public class Base {
     // device on which we want to automate
     // port 4723 is the port APPium server is listening
 
-    File f = new File("/Users/jagmandeepdhaliwal/IdeaProjects/AndroidAutomation/API Demos for Android_v1.9.0_apkpure.com.apk");
-
-    DesiredCapabilities cap = new DesiredCapabilities();
-
-    cap.setCapability(MobileCapabilityType.DEVICE_NAME,"Pixel5");
-
-    cap.setCapability(MobileCapabilityType.APP,);
+        AndroidDriver<AndroidElement> driver;
 
 
+
+        DesiredCapabilities cap= new DesiredCapabilities();
+
+        File appDir = new File("src");
+
+        File app = new File(appDir, "APIDemos.apk");
+
+        cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel5");
+
+
+
+        cap.setCapability(MobileCapabilityType.AUTOMATION_NAME,"uiautomator2");//new step
+
+        cap.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+
+        driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+
+
+
+        driver.closeApp();
     }
 
 }
